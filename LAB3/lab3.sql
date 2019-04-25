f67e45006125bb86176dccb0fe820f2279d8d35c
yaninahusarevych <yanina.husarevych@gmail.com>
2019-04-25 22:06:09 +0300

    Lab3

---
+++ LAB3/lab3.sql
@@ -0,0 +1,19 @@
+USE  passportcreation;
+
+ALTER TABLE passport
+	MODIFY COLUMN type_of_passport ENUM('tourist', 'regular') NOT NULL,
+	MODIFY COLUMN record_number VARCHAR(20) NOT NULL,
+    ADD COLUMN passport_number VARCHAR(8) NOT NULL, 
+    ADD CONSTRAINT UNIQUE UQ_record_no (record_number, passport_number);
+
+ALTER TABLE stuff
+	ADD COLUMN former_position VARCHAR(100) NOT NULL AFTER former_patronymic;
+
+ALTER TABLE documents
+	ADD COLUMN age_version SMALLINT(4) NOT NULL,
+    ADD CONSTRAINT UNIQUE UQ_age_version (age_version); 
+
+DESCRIBE passport;
+DESCRIBE stuff;
+DESCRIBE documents;
+    

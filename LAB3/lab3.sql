c99922e453bfcd985277b2178adf358365dbf58c
yaninahusarevych <yanina.husarevych@gmail.com>
2019-05-17 02:05:14 +0300

    Some modifying

--- LAB3/lab3.sql
+++ LAB3/lab3.sql
@@ -4,7 +4,8 @@ ALTER TABLE passport
 	MODIFY COLUMN type_of_passport ENUM('tourist', 'regular') NOT NULL,
 	MODIFY COLUMN record_number VARCHAR(20) NOT NULL,
     ADD COLUMN passport_number VARCHAR(8) NOT NULL, 
-    ADD CONSTRAINT UNIQUE UQ_record_no (record_number, passport_number);
+    ADD CONSTRAINT UNIQUE UQ_record_no (record_number),
+    ADD CONSTRAINT UNIQUE UQ_passport_no (passport_number);
 
 ALTER TABLE stuff
 	ADD COLUMN former_position VARCHAR(100) NOT NULL AFTER former_patronymic;
